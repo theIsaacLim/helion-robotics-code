cb6bb48de527137536bdbd1ddd6c180af5ae8303
Soundscopez <andrew.yang_203@outlook.com>
2019-02-14 14:36:44 +0800

    Changed all robotmap variables to final

--- Helion_Code_Test_Tank/Testing_Tank/src/main/java/frc/robot/RobotMap.java
+++ Helion_Code_Test_Tank/Testing_Tank/src/main/java/frc/robot/RobotMap.java
@@ -1,26 +1,26 @@
 package frc.robot;
 
 public class RobotMap{
-    public static int flChannel = 0;
-    public static int frChannel = 2;
-    public static int blChannel = 1;
-    public static int brChannel = 3;
+    public static final int flChannel = 0;
+    public static final int frChannel = 2;
+    public static final int blChannel = 1;
+    public static final int brChannel = 3;
 
-    public static int majElevatorChannel = 4;
-    public static int minElevatorChannel = 5;
+    public static final int majElevatorChannel = 4;
+    public static final int minElevatorChannel = 5;
 
-    public static int mainGrabber = 6;
-    public static int slaveGrabber = 7;
+    public static final int mainGrabber = 6;
+    public static final int slaveGrabber = 7;
 
-    public static int joyChannel = 0;
-    public static int joyShoot = 1;
-    public static int joySucc = 2;
-    public static int joySensitivitySlider = 3;
-    public static int joyMajorElevatorUp = 5;
-    public static int joyMajorElevatorDown = 3;
-    public static int joyMinorElevatorUp = 6;
-    public static int joyMinorElevatorDown = 4;
+    public static final int joyChannel = 0;
+    public static final int joyShoot = 1;
+    public static final int joySucc = 2;
+    public static final int joySensitivitySlider = 3;
+    public static final int joyMajorElevatorUp = 5;
+    public static final int joyMajorElevatorDown = 3;
+    public static final int joyMinorElevatorUp = 6;
+    public static final int joyMinorElevatorDown = 4;
 
-    public static int lSMajEleUp = 0;
-    public static int lSMajEleDown = 1;
+    public static final int lSMajEleUp = 0;
+    public static final int lSMajEleDown = 1;
 }
