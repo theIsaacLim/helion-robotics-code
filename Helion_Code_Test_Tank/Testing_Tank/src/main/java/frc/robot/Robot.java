7a125d777d257ca7f5c84b8c75aeb4490add18ab
Soundscopez <andrew.yang_203@outlook.com>
2019-02-14 10:23:01 +0800

    deleted useless comments

--- Helion_Code_Test_Tank/Testing_Tank/src/main/java/frc/robot/Robot.java
+++ Helion_Code_Test_Tank/Testing_Tank/src/main/java/frc/robot/Robot.java
@@ -1,10 +1,3 @@
-/*----------------------------------------------------------------------------*/
-/* Copyright (c) 2017-2018 FIRST. All Rights Reserved.                        */
-/* Open Source Software - may be modified and shared by FRC teams. The code   */
-/* must be accompanied by the FIRST BSD license file in the root directory of */
-/* the project.                                                               */
-/*----------------------------------------------------------------------------*/
-
 package frc.robot;
 
 import com.ctre.phoenix.motorcontrol.InvertType;
@@ -18,10 +11,6 @@ import edu.wpi.first.wpilibj.DigitalInput;
 import edu.wpi.cscore.UsbCamera;
 import edu.wpi.first.cameraserver.CameraServer;
 
-/**
- * This is a demo program showing the use of the RobotDrive class, specifically
- * it contains the code necessary to operate a robot with tank drive.
- */
 public class Robot extends TimedRobot {
 
   WPI_TalonSRX frontLeftMotor;
